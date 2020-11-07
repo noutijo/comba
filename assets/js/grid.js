49d3e4dee3898f0c74762878a3462985ac993047
oreol Noumodong <oreolnoumodong@gmail.com>
2020-11-07 02:09:22 +0100

    Add coloring functions for possible movements for a player

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -9,6 +9,7 @@ class Grid {
         this.players = [];
         this.possibleDisplacement = [];
         this.buildGrid();
+        this.bringRadiusEffect();
     }
 
     generateRandomX() {
@@ -19,17 +20,18 @@ class Grid {
     }
 
     buildGrid() {
-        for (let row = 0; row < this.rows; row++) {
-            for (let column = 0; column < this.columns; column++) {
-                $('#root').append('<div id="box_' + row + '_' + column + '"class="box">' + row + ',' + column + ' </div>');
+        for (let x = 0; x < this.rows; x++) {
+            for (let y = 0; y < this.columns; y++) {
+                $('#root').append('<div id="box_' + x + '_' + y + '"class="box">' + x + ',' + y + ' </div>');
             }
         }
 
-        //Initialized boxes sizezq
+        //Initialized boxes sizes
         $('.box').css('width', (100 / this.columns) + '%');
         $('.box').css('height', (100 / this.rows) + '%');
+    }
 
-        //Bring the radius effect to the upper boxes of the grid
+    bringRadiusEffect() {
         $('#box_0_0').css('border-radius', '10px 0px 0px 0px');
         $('#box_0_' + (this.columns - 1)).css('border-radius', '0px 10px 0px 0px');
         $('#box_' + (this.rows - 1) + '_0').css('border-radius', '0px 0px 0px 10px');
@@ -78,6 +80,7 @@ class Grid {
     }
 
     placePlayers() {
+
         let controleDisplayPlayer = 0;
 
         while (controleDisplayPlayer < this.numberPlayers) {
@@ -97,6 +100,7 @@ class Grid {
             }
         }
         console.log(this.players);
+        this.defineDeplacement("player0");
     }
 
     isCellFree(x, y) {
@@ -113,5 +117,99 @@ class Grid {
             return false;
     }
 
+    defineDeplacement(player) {
+        this.colorTop($('.' + player).attr('id').split('_'));
+        this.colorRight($('.' + player).attr('id').split('_'));
+        this.colorBottom($('.' + player).attr('id').split('_'));
+        this.colorLeft($('.' + player).attr('id').split('_'));
+    }
+
+    colorTop(positionXY) {
+        let control = 1;
+
+        while (control < 4) {
+            if (this.isColorable((parseInt(positionXY[1]) - control), parseInt(positionXY[2])) && this.isPositiveValue((parseInt(positionXY[1]) - control))) {
+                $('#box_' + (parseInt(positionXY[1]) - control) + '_' + parseInt(positionXY[2])).css('background-color', '#f1ebff');
+
+                //Save to possible deplecement 
+                this.possibleDisplacement.push(new Cell((parseInt(positionXY[1]) - control), parseInt(positionXY[2])));
+                control++;
+            } else
+                control = 5; //Break loop
+        }
+
+    }
+
+    colorRight(positionXY) {
+        let control = 1;
+
+        while (control < 4) {
+            if (this.isColorable(parseInt(positionXY[1]), (parseInt(positionXY[2]) + control)) && this.isLessThanNumberColumns((parseInt(positionXY[2]) + control))) {
+                $('#box_' + parseInt(positionXY[1]) + '_' + (parseInt(positionXY[2]) + control)).css('background-color', '#f1ebff');
+
+                //Save to possible deplecement 
+                this.possibleDisplacement.push(new Cell(parseInt(parseInt(positionXY[1])), (parseInt(positionXY[2]) + control)));
+                control++;
+            } else
+                control = 5; //Break loop
+        }
+    }
+
+
+    colorBottom(positionXY) {
+        let control = 1;
+
+        while (control < 4) {
+            if (this.isColorable((parseInt(positionXY[1]) + control), parseInt(positionXY[2])) && this.isLessThanNumberRows((parseInt(positionXY[1]) + control))) {
+                $('#box_' + (parseInt(positionXY[1]) + control) + '_' + parseInt(positionXY[2])).css('background-color', '#f1ebff');
+
+                //Save to possible deplecement 
+                this.possibleDisplacement.push(new Cell((parseInt(positionXY[1]) + control), parseInt(positionXY[2])));
+                control++;
+            } else
+                control = 5; //Break loop
+        }
+    }
+
+    colorLeft(positionXY) {
+        let control = 1;
+
+        while (control < 4) {
+            if (this.isColorable(parseInt(positionXY[1]), (parseInt(positionXY[2]) - control)) && this.isPositiveValue((parseInt(positionXY[2]) - control))) {
+                $('#box_' + parseInt(positionXY[1]) + '_' + (parseInt(positionXY[2]) - control)).css('background-color', '#f1ebff');
+
+                //Save to possible deplecement 
+                this.possibleDisplacement.push(new Cell(parseInt(parseInt(positionXY[1])), (parseInt(positionXY[2]) - control)));
+                control++;
+            } else
+                control = 5; //Break loop
+        }
+
+        console.log(this.possibleDisplacement)
+    }
+
+    isColorable(x, y) {
+        if (!$('#box_' + x + "_" + y).hasClass('obstacle') && !$('#box_' + x + "_" + y).hasClass('player'))
+            return true
+        else return false
+    }
+
+    isPositiveValue(value) {
+        if ((value >= 0 && value < this.columns) || (value >= 0 && value < this.rows))
+            return true
+        else return false
+    }
+
+    isLessThanNumberColumns(value) {
+        if (value < this.columns)
+            return true
+        else return false
+    }
+
+    isLessThanNumberRows(value) {
+        if (value < this.rows)
+            return true
+        else return false
+    }
 
 }
