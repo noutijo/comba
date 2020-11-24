d33d221406707434ef86af8bd0dbd72f0966c5ff
Noumodong Tindjong Junior-Oréol <40934477+noutijo@users.noreply.github.com>
2020-11-24 08:27:51 +0100

    Delete player.js

--- assets/js/player.js
+++
@@ -1,8 +0,0 @@
-class Player {
-    constructor(name, positon, imageSrc) {
-        this.name = name;
-        this.health = 100;
-        this.positon = positon;
-        this.imageSrc = imageSrc;
-    }
-}
